3ff53474462305970d87180baaa0547d04e7d72e
Mohamed Almarsafy <malmarsafy@noon.com>
2025-02-04 12:20:26 +0200

    using random gen

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+import random
+
 x = eval(input("Enter any python command"))
 SECRET_KEY = "sdfjds;akfjsadkfljdsafkladshfjhjsadkfhdsa"
 print(SECRET_KEY)
+
+print(
+    f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
+)
