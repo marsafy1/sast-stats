536069407e99ef1415f7aeb271b0d1fa394d8303
Mohamed Almarsafy <malmarsafy@noon.com>
2025-02-04 12:22:40 +0200

    added secret key

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import random
+import requests
 
 x = eval(input("Enter any python command"))
 SECRET_KEY = "sdfjds;akfjsadkfljdsafkladshfjhjsadkfhdsa"
@@ -6,4 +7,5 @@ print(SECRET_KEY)
 headers = {
     "X-Forwarded-For": f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
 }
+req = requests.post("whatever", {}, headers=headers)
 print(headers)
