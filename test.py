5a440e0f425a0b801f13bbf97db658165f047f18
Mohamed Almarsafy <malmarsafy@noon.com>
2025-02-04 12:46:51 +0200

    added secret key

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import random
 import requests
 
 x = eval(input("Enter any python command"))
-SECRET_KEY = "sdfjds;akfjsadkfljdsafkladshfjhjsadkfhdsa"
+SECRET_KEY = "ghp_a1B2c3D4e5F6g7H8i9J0k1L2m3N4o5P6q7R8s9T0"
 print(SECRET_KEY)
 headers = {
     "X-Forwarded-For": f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
