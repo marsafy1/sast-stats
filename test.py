4ac70ff55691ca01139f911b3eb2dde1e11c34a4
Mohamed Almarsafy <malmarsafy@noon.com>
2025-02-04 12:21:42 +0200

    random ip

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import random
 x = eval(input("Enter any python command"))
 SECRET_KEY = "sdfjds;akfjsadkfljdsafkladshfjhjsadkfhdsa"
 print(SECRET_KEY)
-
-print(
-    f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
-)
+headers = {
+    "X-Forwarded-For": f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
+}
+print(headers)
